005d9a7cb4e1c4d38a48cad398ece6d35640a033
Yakirza1 <yakirz1@gmail.com>
2019-12-16 15:47:11 +0200

    Json

--- src/main/java/bgu/spl/mics/application/MI6Runner.java
+++ src/main/java/bgu/spl/mics/application/MI6Runner.java
@@ -1,68 +1,42 @@
 package bgu.spl.mics.application;
 
 import bgu.spl.mics.application.passiveObjects.Inventory;
-import com.google.gson.Gson;
-import java.io.IOException;
-import com.google.gson.GsonBuilder;
-import javax.xml.transform.Result;
-import java.io.FileNotFoundException;
+import com.google.gson.*;
+
+import javax.xml.transform.*;
+
+import java.lang.reflect.*;
+
 import java.io.FileReader;
-import java.lang.reflect.Type;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonParser;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.nio.file.*;
+import java.util.concurrent.ExecutionException;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import jdk.internal.vm.compiler.collections.EconomicMap;
 
 /** This is the Main class of the application. You should parse the input file,
  * create the different instances of the objects, and run the system.
  * In the end, you should output serialized objects.
  */
 public class MI6Runner {
-    public static void main(String[] args) throws FileNotFoundException {
-        Inventory response = null;
-        //Json
-      /*  Gson gson = new Gson();
+    public static void main(String[] args) {
+        try {
+            //Json
 
-        String[] anotherStr = gson.fromJson( , String[].class);
+            FileReader reader = new FileReader(
+                    "C/Users/yakirzagron/Downloads/MI6c/src/input201 - 2.json");
+            JsonElement jsonElement = new JsonParser().parse(reader);
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            JsonArray gag = jsonObject.get("inventory").getAsJsonArray();
 
-        System.out.println(anotherStr[1]);
 
-*/
-      List<String> as = new LinkedList<>();
 
-       Gson gson = new Gson();
-        FileReader br = new FileReader(
-              "C:\\Users\\Yakir\\Desktop\\MI6c\\input201 - 2.json");
-      ;
-       // for (int i = 0; i < jsonArray.size(); i++) {
-         //   JsonElement str = gson.get(1);
-         //   String[] anotherStr = gson.fromJson(br, String[].class);
-           // System.out.println(anotherStr[1]);
-         //   System.out.println(br);
-       // }
-        //final JsonElement dataElement = obj.get("data");
-        /*
-       Gson gson = new Gson();
-       response = gson.fromJson( br, Inventory.class);
-        if(response != null){
-            for(String h :  response.getGadgets()){
-                System.out.println(h);
-            }
-
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        else {
-            System.out.println("good Night");
-        }
-*/
+
     }
 }
